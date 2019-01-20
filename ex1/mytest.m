e3d0c3b29e7a60c51c5e7ec21a22328af11d1e23
manabukato <manabu.kato@g.softbank.co.jp>
2019-01-20 11:42:50 +0900

    Add files via upload

---
+++ ex1/mytest.m
@@ -0,0 +1,13 @@
+data = load('ex1data1.txt');
+X = data(:, 1);
+y = data(:, 2);
+m = length(y);
+
+plot(X, y, 'rx', 'MarkerSize', 10);
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+iterations = 1500;
+alpha = 0.01;
+
+
